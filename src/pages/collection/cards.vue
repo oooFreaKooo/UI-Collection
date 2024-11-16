27f0e09832eec2d72d9f8e5fcf535ddebb5939c0
Roman Koller <koller.roman@mail.ru>
2024-11-16 15:00:52 +0100

    Add GradientBorderHover

--- src/pages/collection/cards.vue
+++ src/pages/collection/cards.vue
@@ -28,7 +28,7 @@ const showCodeDialog = ref(false)
 const selectedComponent = ref<string>('')
 const selectedComponentInstance = ref(null)
 const componentOptions = [
-    'FuturisticCard', 'EvervaultHover', 'TrippyCardBgChange', 'SwipeHoverCards',
+    'FuturisticCard', 'EvervaultHover', 'TrippyCardBgChange', 'SwipeHoverCards', 'GradientBorderHover',
 ]
 
 // Watch for component selection changes
