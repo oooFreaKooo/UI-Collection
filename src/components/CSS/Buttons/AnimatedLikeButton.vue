8f2998410d0c2889842a6ad67b9455a26fb2372a
Roman Koller <koller.roman@mail.ru>
2024-11-17 19:23:41 +0100

    adjust buttons styles

--- src/components/CSS/Buttons/AnimatedLikeButton.vue
+++ src/components/CSS/Buttons/AnimatedLikeButton.vue
@@ -17,14 +17,14 @@
                         v-if="!isFilled"
                         id="heart"
                         :class="{ 'fill-animation': isAnimating }"
-                        fill="white"
+                        fill="currentColor"
                         d="M16.697 5.5c-1.222-.06-2.679.51-3.89 2.16l-.805 1.09-.806-1.09C9.984 6.01 8.526 5.44 7.304 5.5c-1.243.07-2.349.78-2.91 1.91-.552 1.12-.633 2.78.479 4.82 1.074 1.97 3.257 4.27 7.129 6.61 3.87-2.34 6.052-4.64 7.126-6.61 1.111-2.04 1.03-3.7.477-4.82-.561-1.13-1.666-1.84-2.908-1.91zm4.187 7.69c-1.351 2.48-4.001 5.12-8.379 7.67l-.503.3-.504-.3c-4.379-2.55-7.029-5.19-8.382-7.67-1.36-2.5-1.41-4.86-.514-6.67.887-1.79 2.647-2.91 4.601-3.01 1.651-.09 3.368.56 4.798 2.01 1.429-1.45 3.146-2.1 4.796-2.01 1.954.1 3.714 1.22 4.601 3.01.896 1.81.846 4.17-.514 6.67z"
                     />
                     <path
                         v-if="isFilled"
                         id="x"
                         style="visibility: hidden;"
-                        fill="white"
+                        fill="currentColor"
                         d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"
                     />
                     <path
@@ -57,31 +57,32 @@ const toggleAnimation = () => {
 
 <style scoped>
 .main-body {
-  background-color: black;
-  position: relative;
-  top: 0;
-  display: flex;
-  justify-content: center;
-  align-items: flex-start;
-  transform: scale(1.8);
-  transform-origin: top center;
-  padding-top: 20px;
+  margin-left: auto;
+  margin-right: auto;
+  text-align: center;
 }
 
 #button {
-  all: unset;
+  color: currentColor;
   width: 30px;
   cursor: pointer;
-  position: relative;
   -webkit-tap-highlight-color: transparent;
 }
 
-/* Add a scaling effect to simulate GSAP's scaling */
 .svg-scale {
   animation: scale-animation 0.5s ease forwards;
 }
 
-/* Scaling animation */
+.fill-animation {
+  fill: rgb(249, 24, 128);
+  transition: fill 0.25s ease;
+}
+
+.opacity-animation {
+  opacity: 1;
+  transition: opacity 0.25s ease;
+}
+
 @keyframes scale-animation {
   0% {
     transform: scale(1);
@@ -92,17 +93,5 @@ const toggleAnimation = () => {
   100% {
     transform: scale(1);
   }
-}
-
-/* Fill color transition for heart */
-.fill-animation {
-  fill: rgb(249, 24, 128);
-  transition: fill 0.25s ease;
-}
-
-/* Opacity animation for heart-filled */
-.opacity-animation {
-  opacity: 1;
-  transition: opacity 0.25s ease;
 }
 </style>
