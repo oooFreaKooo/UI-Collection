1f877e5104f0f843791ea683a122ecaf768f2362
Roman Koller <koller.roman@mail.ru>
2024-11-21 14:33:06 +0100

    Add 3DToggle

--- src/pages/collection/buttons.vue
+++ src/pages/collection/buttons.vue
@@ -29,7 +29,7 @@ const showCodeDialog = ref(false)
 const selectedComponent = ref<string>('')
 const selectedComponentInstance = ref(null)
 const componentOptions = [
-    'AnimatedToggle', 'MobileMenuToggle', 'AnimatedLikeButton', 'AnimatedCheckbox',
+    'AnimatedToggle', '3DToggle', 'MobileMenuToggle', 'AnimatedLikeButton', 'AnimatedCheckbox',
 ]
 
 // Watch for component selection changes
